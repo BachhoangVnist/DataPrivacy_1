468c4f3c7ff7e3f2d40c730c37a19c2cb1ce0291
bachhx-0399 <hoang.xuan.bach@sun-asterisk.com>
2024-03-06 13:59:04 +0700

    add pylint convention to project

--- qabot.py
+++ qabot.py
@@ -9,41 +9,45 @@ from llms import llm_model
 
 # Tao prompt template
 def creat_prompt(template):
-    prompt = PromptTemplate(template = template, input_variables=["context", "question"])
+    prompt = PromptTemplate(template=template, input_variables=["context", "question"])
     return prompt
 
 # Tao simple chain
 def create_qa_chain(prompt, llm, db: VectorStore):
     llm_chain = RetrievalQA.from_chain_type(
-        llm = llm,
-        chain_type= "stuff",
-        retriever = db.as_retriever(search_kwargs = {
-            "k":1, }),
-        return_source_documents = False,
-        chain_type_kwargs= {'prompt': prompt}
+        llm=llm,
+        chain_type="stuff",
+        retriever=db.as_retriever(
+            search_kwargs={
+                "k": 1, 
+            }
+        ),
+        return_source_documents=False,
+        chain_type_kwargs={'prompt': prompt}
 
     )
     return llm_chain
 
 # Read from VectorDB
 def read_vectors_db():
-    vectorStore = MongoDBAtlasVectorSearch(
+    vector_store = MongoDBAtlasVectorSearch(
         vietnamese_legal_collection,
         embedding_model,
         index_name=VECTOR_SEARCH_INDEX_NAME
     )
-    return vectorStore
+    return vector_store
+
 
 if __name__ == "__main__":
     # Bat dau thu nghiem
     # db = read_vectors_db()
-    # output = db.similarity_search_with_score(query="Quy định về cơ sở vật chất đối với môn đấu kiếm thể thao?", 
+    # output = db.similarity_search_with_score(query="Quy định về cơ sở vật chất đối với môn đấu kiếm thể thao?",
     #                                          k=2,
     #                                          post_filter_pipeline=  [
     #                                             {
     #                                                 "$project": {
     #                                                     "_id": 0,
-    #                                                     "text": 1, 
+    #                                                     "text": 1,
     #                                                     "score": { "$meta": "vectorSearchScore" }
     #                                                 }
     #                                             }],)
@@ -54,17 +58,17 @@ if __name__ == "__main__":
     start_time = time.time()
 
     db = read_vectors_db()
-    #Tao Prompt
-    template = """<|im_start|>system\nSử dụng thông tin sau đây để trả lời câu hỏi. Nếu bạn không biết câu trả lời, hãy nói không biết, đừng cố tạo ra câu trả lời\n
+    # Tao Prompt
+    TEMPLATE = """<|im_start|>system\nSử dụng thông tin sau đây để trả lời câu hỏi. Nếu bạn không biết câu trả lời, hãy nói không biết, đừng cố tạo ra câu trả lời\n
         {context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assistant"""
-    prompt = creat_prompt(template)
+    prompt = creat_prompt(TEMPLATE)
 
-    llm_chain  =create_qa_chain(prompt, llm_model, db)
+    llm_chain = create_qa_chain(prompt, llm_model, db)
 
     # Chay cai chain
-    question = "Quy định về cơ sở vật chất đối với môn đấu kiếm thể thao?"
+    QUESTION = "Quy định về cơ sở vật chất đối với môn đấu kiếm thể thao?"
     print('start invoke')
-    response = llm_chain.invoke({"query": question})
+    response = llm_chain.invoke({"query": QUESTION})
     print(response)
 
     # Kết thúc đo thời gian
